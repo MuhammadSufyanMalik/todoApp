d1051462632ae7b278d9ca0d042ee2e39dce8936
MuhammadSufyanMalik <muhammad.sufyan@ardgrup.com>
2022-05-24 16:26:21 +0300

    3 Model_Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Malik';
+  items = [
+    { description: 'Kahvalti', action: 'No' },
+    { description: 'Fatura Ödeme', action: 'No' },
+    { description: 'Spor', action: 'No' },
+    { description: 'Sinema', action: 'No' }
+  ];
 }
